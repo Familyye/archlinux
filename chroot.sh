268725ad338d9a087f576572e5481abe4f0652f4
wangzhl <578246786@qq.com>
2019-01-11 18:29:58 +0800

    add user

--- chroot.sh
+++ chroot.sh
@@ -11,8 +11,15 @@ locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 echo -e "127.0.0.1	localhost\n::1		localhost" > /etc/hosts
+
+#user
 echo "set root password"
 passwd
+echo "add general user"
+read generaluser
+useradd $generaluser
+echo "set user password"
+passwd $generaluser
 
 #grub
 yes | pacman -S grub
